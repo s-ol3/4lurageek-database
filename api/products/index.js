d035c003f894d3d5b5166a9595944d4f3bd55baa
sol3 <marzen25@gmail.com>
2024-05-21 21:03:42 -0300

    cors index.js

--- api/products/index.js
+++ api/products/index.js
@@ -1,4 +1,27 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir acceso desde cualquier origen (cambia '*' por el origen específico de tu aplicación web si es necesario)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT'); // Permitir métodos GET y PUT
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Permitir el encabezado Content-Type
+  res.setHeader('Content-Type', 'application/json'); // Establecer el tipo de contenido de la respuesta como JSON
+
+  if (req.method === 'GET') {
+    // Lógica para obtener los productos
+    const data = require('./products.json');
+    res.status(200).json(data);
+  } else if (req.method === 'PUT') {
+    // Lógica para actualizar los productos
+    const newData = JSON.parse(req.body);
+    // Guardar newData en el archivo products.json
+    require('fs').writeFileSync('./api/products/products.json', JSON.stringify(newData));
+    res.status(200).end();
+  } else {
+    res.status(405).end(); // Método no permitido
+  }
+}
+
+
+
+/* export default function handler(req, res) {
     if (req.method === 'GET') {
       // Lógica para obtener los productos
       const data = require('./products.json');
@@ -12,5 +35,5 @@ export default function handler(req, res) {
     } else {
       res.status(405).end(); // Método no permitido
     }
-  }
+  } */
   
