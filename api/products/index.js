dab7462c69a3ce144bc563506663305752176195
sol3 <marzen25@gmail.com>
2024-05-21 21:19:05 -0300

    cualquier origen *

--- api/products/index.js
+++ api/products/index.js
@@ -1,4 +1,31 @@
 export default function handler(req, res) {
+  // Permitir solicitudes desde cualquier origen
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Permitir métodos GET, PUT y OPTIONS
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, OPTIONS');
+  // Permitir el encabezado Content-Type
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    // Responder con OK para las solicitudes OPTIONS
+    res.status(200).end();
+  } else if (req.method === 'GET') {
+    // Lógica para obtener los productos
+    const data = require('./products.json');
+    res.status(200).json(data);
+  } else if (req.method === 'PUT') {
+    // Lógica para actualizar los productos
+    const newData = JSON.parse(req.body);
+    // Guardar newData en el archivo products.json
+    require('fs').writeFileSync('./api/products/products.json', JSON.stringify(newData));
+    res.status(200).end();
+  } else {
+    res.status(405).end(); // Método no permitido
+  }
+}
+
+
+/* export default function handler(req, res) {
   // Permitir solicitudes desde cualquier origen (cambia '*' por el origen específico de tu aplicación web si es necesario)
   res.setHeader('Access-Control-Allow-Origin', 'https://4lurageek-database.vercel.app/');
   // Permitir métodos GET, PUT y OPTIONS
@@ -22,7 +49,7 @@ export default function handler(req, res) {
   } else {
     res.status(405).end(); // Método no permitido
   }
-}
+} */
 
 
 /* export default function handler(req, res) {
