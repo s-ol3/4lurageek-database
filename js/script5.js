74e3f6000674498a3eb2d52c06b5a72ab77675af
sol3 <marzen25@gmail.com>
2024-05-22 00:16:10 -0300

    links cors

--- js/script5.js
+++ js/script5.js
@@ -64,7 +64,7 @@ function eliminarProducto(event) {
 
   fetch(productosData, {
     method: 'PUT',
-    mode: 'no-cors',  //------------------------------------------------
+
     headers: {
       'Content-Type': 'application/json'
     },
